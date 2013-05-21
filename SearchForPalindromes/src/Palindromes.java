f9038dd26fea0023e7d6ee0166d0cd4adeb962e8
ldimitrova <liliya.dimitrova90@gmail.com>
2013-05-21 08:51:41 +0300

    Format

--- SearchForPalindromes/src/Palindromes.java
+++ SearchForPalindromes/src/Palindromes.java
@@ -1,42 +1,44 @@
-
 public class Palindromes {
 
-	public static void returnPalyndroms(String str){
+	public static void returnPalyndroms(String str) {
 		StringBuilder palindrome = new StringBuilder();
 		String lastPalindrome = "";
 		int startIndex;
 		int lastIndex;
 		char ch;
-		
+
 		for (int i = 0; i < str.length(); i++) {
 			startIndex = i;
 			ch = Character.toLowerCase(str.charAt(i));
-			
-			lastIndex = str.lastIndexOf(ch, str.length()-1);
-			
-			while(startIndex < lastIndex){
-				String currentString = str.substring(startIndex, lastIndex+1);
-			
-				for (int j = currentString.length()-1; j >= 0; j--) {
+
+			lastIndex = str.lastIndexOf(ch, str.length() - 1);
+
+			while (startIndex < lastIndex) {
+				String currentString = str.substring(startIndex, lastIndex + 1);
+
+				for (int j = currentString.length() - 1; j >= 0; j--) {
 					palindrome.append(currentString.charAt(j));
 				}
-				
-				if((currentString.toLowerCase().equals(palindrome.toString().toLowerCase())) &&
-					(!lastPalindrome.toLowerCase().contains(currentString.toLowerCase()))) {
-					
+
+				if ((currentString.toLowerCase().equals(palindrome.toString()
+						.toLowerCase()))
+						&& (!lastPalindrome.toLowerCase().contains(
+								currentString.toLowerCase()))) {
+
 					lastPalindrome = currentString;
 					System.out.println(currentString);
 				}
 				palindrome.setLength(0);
 				lastIndex = str.lastIndexOf(ch, lastIndex - 1);
-				
+
 			}
-			
+
 		}
-		
+
 	}
+
 	public static void main(String[] args) {
-		returnPalyndroms("ThisisAlabalanicaneven"); 
-		
+		returnPalyndroms("ThisisAlabalanicaneven");
+
 	}
 }
